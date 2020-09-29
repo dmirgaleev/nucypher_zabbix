a6c4bcb676c8dd0606fe024f89a4cd4b8ef3a13e
dmirgaleev <35151170+dmirgaleev@users.noreply.github.com>
2020-09-29 17:39:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,12 +13,11 @@ STAKERADDRESS=`cat $HOME1/.local/share/nucypher/ursula.json | jq .checksum_addre
 crontab -l | { cat; echo "*/5 * * * * su $USR1 -c 'cd ~ nucypher status stakers --provider ~/.ethereum/geth.ipc > /tmp/nucypher.tmp && cp /tmp/nucypher.tmp /tmp/nucypher.txt'"; } | crontab -
 crontab -l | { cat; echo "*/5 * * * * su $USR1 -c 'cd ~ nucypher worklock status --provider ~/.ethereum/geth.ipc --bidder-address $STAKERADDRESS > /tmp/worklock.tmp && cp /tmp/worklock.tmp /tmp/worklock.txt'"; } | crontab -
 mkdir -p /etc/zabbix/scripts
-curl -s https://raw.githubusercontent.com/sl-he/nucypher_zabbix/master/geth.sh > /etc/zabbix/scripts/geth.sh
-curl -s https://raw.githubusercontent.com/sl-he/nucypher_zabbix/master/nucypher-stats.sh > /etc/zabbix/scripts/nucypher-stats.sh
-curl -s https://raw.githubusercontent.com/sl-he/nucypher_zabbix/master/nucypher-version.sh > /etc/zabbix/scripts/nucypher-version.sh
-curl -s https://raw.githubusercontent.com/sl-he/nucypher_zabbix/master/worklock.sh > /etc/zabbix/scripts/worklock.sh
-curl -s https://raw.githubusercontent.com/sl-he/nucypher_zabbix/master/nucypher.conf > /etc/zabbix/zabbix_agentd.d/nucypher.conf
-curl -s https://raw.githubusercontent.com/sl-he/nucypher_zabbix/master/zabbix_agentd.conf  > /etc/zabbix/zabbix_agentd.conf
+curl -s https://raw.githubusercontent.com/dmirgaleev/nucypher_zabbix/master/geth.sh > /etc/zabbix/scripts/geth.sh
+curl -s https://raw.githubusercontent.com/dmirgaleev/nucypher_zabbix/master/nucypher-stats.sh > /etc/zabbix/scripts/nucypher-stats.sh
+curl -s https://raw.githubusercontent.com/dmirgaleev/nucypher_zabbix/master/nucypher-version.sh > /etc/zabbix/scripts/nucypher-version.sh
+curl -s https://raw.githubusercontent.com/dmirgaleev/nucypher_zabbix/master/worklock.sh > /etc/zabbix/scripts/worklock.sh
+curl -s https://raw.githubusercontent.com/dmirgaleev/nucypher_zabbix/master/nucypher.conf > /etc/zabbix/zabbix_agentd.d/nucypher.conf
 chmod 700 /etc/zabbix/scripts/*.sh
 service zabbix-agent restart
 service zabbix-agent status
